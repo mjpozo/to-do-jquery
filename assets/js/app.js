e68d55ee87f824ab30710c2741ce9eebbe10e8e8
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-06-29 18:13:19 -0400

    se muestran las tareas en ambos items, la idea es ocultar uno y mostrar otro cuando se clickea el check

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,24 +5,27 @@ $(document).ready(function(){
 	$("#send").click(function(e){
 
 		e.preventDefault();
-
 		i++;
 
 		var tarea = $("#work").val();
 
 		if (tarea != ""){
-			$("#por-hacer").append('<div class="row" id="row' + i.toString() + '"></div>');
+			$("#por-hacer").append('<div class="row" id="rowDo' + i.toString() + '"></div>');
+			$("#hecho").append('<div class="row" id="rowDone' + i.toString() + '"></div>');
 
-			$("#row" + i.toString()).append('<div class="col s10 cont-check" id="cont-check' + i.toString() + '"></div>');
-			$("#cont-check" + i.toString()).append('<input type="checkbox" class="check" id="check' + i.toString() + '" value="miCheck' + i.toString() + '">');
+			$("#rowDo" + i.toString()).append('<div class="col s10 cont-check" id="cont-check-do' + i.toString() + '"></div>');
+			$("#cont-check-do" + i.toString()).append('<input type="checkbox" class="check" id="check-do' + i.toString() + '" value="miCheck-do' + i.toString() + '">');
+			$("#cont-check-do" + i.toString()).append('<label for="check-do' + i.toString() + '">' + tarea + '</label>');
 
-			$("#cont-check" + i.toString()).append('<label for="check' + i.toString() + '">' + tarea + '</label>');
+			$("#rowDone" + i.toString()).append('<div class="col s10 cont-check" id="cont-check-done' + i.toString() + '"></div>');
+			$("#cont-check-done" + i.toString()).append('<input type="checkbox" class="check" id="check-done' + i.toString() + '" value="miCheck-do' + i.toString() + '">');
+			$("#cont-check-done" + i.toString()).append('<label for="check-done' + i.toString() + '">' + tarea + '</label>');
 
-			//$("#row" + i.toString()).append('<div class="col offset-s2 s8" id="cont-work' + i.toString() + '"></div>');
-			//$("#cont-work" + i.toString()).append('<p>' + tarea + '</p>');
+			$("#rowDo" + i.toString()).append('<div class="col s2" id="cont-btn-do' + i.toString() + '"></div>');
+			$("#cont-btn-do" + i.toString()).append('<a class="waves-effect waves-light btn" id="delete-do' + i.toString() + '"><i class="medium material-icons">delete</i></a>');
 
-			$("#row" + i.toString()).append('<div class="col s2" id="cont-btn' + i.toString() + '"></div>');
-			$("#cont-btn" + i.toString()).append('<a class="waves-effect waves-light btn" id="delete' + i.toString() + '"><i class="medium material-icons">delete</i></a>');
+			$("#rowDone" + i.toString()).append('<div class="col s2" id="cont-btn-done' + i.toString() + '"></div>');
+			$("#cont-btn-done" + i.toString()).append('<a class="waves-effect waves-light btn" id="delete-done' + i.toString() + '"><i class="medium material-icons">delete</i></a>');
 
 			document.getElementById("work").value = "";
 
@@ -32,20 +35,27 @@ $(document).ready(function(){
 
 			}*/
 
-
 		}
 
 		//para borrar tareas
-		$("#delete" + i.toString()).click(function(e){
-
+		$("#delete-do" + i.toString()).click(function(e){
 			e.preventDefault();
-
 			$(this).parent().parent().remove();
 		});
 
+		$("#delete-done" + i.toString()).click(function(e){
+			e.preventDefault();
+			$(this).parent().parent().remove();
+		});
+
+		//para los checks
+		if ($("[checked]").val() == "checked"){
+				
+
+			}
+		
+		
 
 	});
-
-	
 
 });
