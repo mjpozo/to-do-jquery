e9aed14a684bdafd1538a46a271163bc9a2a5960
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-06-29 17:25:15 -0400

    ahora el boton delete funciona

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,17 +13,35 @@ $(document).ready(function(){
 		if (tarea != ""){
 			$("#por-hacer").append('<div class="row" id="row' + i.toString() + '"></div>');
 
-			$("#row" + i.toString()).append('<div class="col s1 cont-check" id="cont-check' + i.toString() + '"></div>');
-			$("#cont-check" + i.toString()).append('<input type="checkbox" class="check" value="check' + i.toString() + '">');
+			$("#row" + i.toString()).append('<div class="col s10 cont-check" id="cont-check' + i.toString() + '"></div>');
+			$("#cont-check" + i.toString()).append('<input type="checkbox" class="check" id="check' + i.toString() + '" value="miCheck' + i.toString() + '">');
 
-			$("#row" + i.toString()).append('<div class="col s10" id="cont-work' + i.toString() + '"></div>');
-			$("#cont-work" + i.toString()).append(tarea);
+			$("#cont-check" + i.toString()).append('<label for="check' + i.toString() + '">' + tarea + '</label>');
 
-			$("#row" + i.toString()).append('<div class="col s1" id="cont-btn' + i.toString() + '"></div>');
-			$("#cont-btn" + i.toString()).append('<button type="button">Remove</button>');
+			//$("#row" + i.toString()).append('<div class="col offset-s2 s8" id="cont-work' + i.toString() + '"></div>');
+			//$("#cont-work" + i.toString()).append('<p>' + tarea + '</p>');
+
+			$("#row" + i.toString()).append('<div class="col s2" id="cont-btn' + i.toString() + '"></div>');
+			$("#cont-btn" + i.toString()).append('<a class="waves-effect waves-light btn" id="delete' + i.toString() + '"><i class="medium material-icons">delete</i></a>');
 
 			document.getElementById("work").value = "";
+
+			//cuando se hace check al checkbox
+			/*if ($("[checked]").val() == "checked"){
+				$("#row" + i.toString()).remove();
+
+			}*/
+
+
 		}
+
+		//para borrar tareas
+		$("#delete" + i.toString()).click(function(e){
+
+			e.preventDefault();
+
+			$(this).parent().parent().remove();
+		});
 
 
 	});
