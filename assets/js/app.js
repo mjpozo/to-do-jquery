d35d57a68514533a97386f653a01be78df1db8c4
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-06-30 02:44:13 -0400

    no se pudo lograr la parte del check

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,15 +14,14 @@ $(document).ready(function(){
 
 			if (tarea != ""){
 				$("#por-hacer").append('<div class="row" id="rowDo' + i.toString() + '"></div>');
-				$("#hecho").append('<div class="row" id="rowDone' + i.toString() + '"></div>');
+				$("#hecho").append('<div class="rowDone" id="rowDone' + i.toString() + '"></div>');
 
 				$("#rowDo" + i.toString()).append('<div class="col s10 cont-check" id="cont-check-do' + i.toString() + '"></div>');
-				$("#cont-check-do" + i.toString()).append('<input type="checkbox" class="check" id="check-do' + i.toString() + '" value="miCheck-do' + i.toString() + '">');
+				$("#cont-check-do" + i.toString()).append('<input type="checkbox" class="checkDo" id="check-do' + i.toString() + '" value="miCheck-do' + i.toString() + '">');
 				$("#cont-check-do" + i.toString()).append('<label for="check-do' + i.toString() + '">' + tarea + '</label>');
 
 				$("#rowDone" + i.toString()).append('<div class="col s10 cont-check" id="cont-check-done' + i.toString() + '"></div>');
-				$("#cont-check-done" + i.toString()).append('<input type="checkbox" class="check" id="check-done' + i.toString() + '" value="miCheck-done' + i.toString() + '">');
-				$("#cont-check-done" + i.toString()).append('<label for="check-done' + i.toString() + '">' + tarea + '</label>');
+				$("#cont-check-done" + i.toString()).append('<span>' + tarea + '</span>');
 
 				$("#rowDo" + i.toString()).append('<div class="col s2" id="cont-btn-do' + i.toString() + '"></div>');
 				$("#cont-btn-do" + i.toString()).append('<a class="waves-effect waves-light btn" id="delete-do' + i.toString() + '"><i class="medium material-icons">delete</i></a>');
@@ -32,11 +31,6 @@ $(document).ready(function(){
 
 				document.getElementById("work").value = "";
 
-				//cuando se hace check al checkbox
-				/*if ($("[checked]").val() == "checked"){
-					$("#row" + i.toString()).remove();
-
-				}*/
 			}
 
 		}
@@ -53,10 +47,29 @@ $(document).ready(function(){
 		});
 
 		//para los checks
-		$("#rowDone" + i.toString()).hide();
-		//$(document).on("change",);
-		
-		
+
+		//esto mantiene inicialmente ocultas las tareas en el item completed
+		$(".rowDone").hide();
+
+
+		//cuando de hace check en To Do Items
+		//no funciona por dos razones:
+		//siempre hace que muestre el ultimo item de la lista y ninguno mas
+		//al apretar cualquier boton, vuelve a desaparecer el elemento reciente de completed item
+		$("#check-do" + i.toString()).click(function(e){
+			e.preventDefault();
+			$(this).parent().parent().remove();
+			$("#rowDone" + i.toString()).show();
+		});
+
+
+		//cuando de hace check en To Do Items (NO FUNCIONA)
+		/*$(document).on("change","#check-done" + i.toString(),function(){
+			$("#rowDone" + i.toString()).hide();
+			$("#rowDo" + i.toString()).show();
+
+			$("#check-do" + i.toString()).removeAttr('checked');
+		});*/
 
 	});
 
